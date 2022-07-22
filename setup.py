91b2a6e9ab6f1bf00e24c6f703abde9bb3f75754
vyrjana <41105805+vyrjana@users.noreply.github.com>
2022-07-22 09:58:18 +0300

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api-documenter",
-    version="0.1.0",
+    version="0.2.0",
     author="Ville Yrjänä",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
