072448258e86ba168387a871140a1ba349422409
vyrjana <41105805+vyrjana@users.noreply.github.com>
2022-10-30 10:30:26 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api-documenter",
-    version="0.2.6",
+    version="0.2.7",
     author="Ville Yrjänä",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
